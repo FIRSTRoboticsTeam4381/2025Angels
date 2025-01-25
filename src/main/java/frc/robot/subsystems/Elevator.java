3c94bc74e5d5ea5ada6459f5c403e2327c9a963f
nmwal <nmwalton30@gmail.com>
2025-01-25 14:38:54 -0500

    added smart dashboard to elevator

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -14,6 +14,7 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
@@ -48,7 +49,9 @@ public class Elevator extends SubsystemBase {
 
   @Override
   public void periodic() {
+    SmartDashboard.putData(this);
   }
+  
     // This method will be called once per scheduler run
    
     public Command joystickcontrol(Supplier<Double> joystickMove)
