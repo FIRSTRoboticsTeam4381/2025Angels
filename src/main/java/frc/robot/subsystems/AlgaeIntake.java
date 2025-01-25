2f9037dd157bf3d0fc11716153d5ff889bf96429
BillyBobJones678 <williamholmes678@gmail.com>
2025-01-25 10:26:42 -0500

    Added Sensor and a Command to Intake and Outtake depending on the sensor
    On the Algae Intake Subsystem

--- src/main/java/frc/robot/subsystems/AlgaeIntake.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntake.java
@@ -8,8 +8,10 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -22,11 +24,13 @@ public class AlgaeIntake extends SubsystemBase {
     private SparkMax motor1;
     private SparkMax motor2;
 
+    private DigitalInput sensor;
     public AlgaeIntake() {
         motor1 = new SparkMax(50,MotorType.kBrushless);
         motor2 = new SparkMax(51,MotorType.kBrushless);
-    
+        sensor = new DigitalInput(0);
 
+        
 SparkMaxConfig motor1Config = new SparkMaxConfig();
     
     motor1Config.smartCurrentLimit(25)
@@ -41,7 +45,7 @@ SparkMaxConfig motor2Config = new SparkMaxConfig();
     motor2Config.apply(motor1Config);
     motor2Config.follow(motor1, true);
 
-motor2.configure(motor2Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motor2.configure(motor2Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     
  }  
     
@@ -56,7 +60,17 @@ motor2.configure(motor2Config, ResetMode.kResetSafeParameters, PersistMode.kPers
                 new InstantCommand(()-> motor1.set(-1), this),
                 new WaitCommand(2),
                 new InstantCommand(()-> motor1.set(0),this)
-            );                
-        } 
+            ).withName("AlgaeIntake");  
+         } 
+         public Command Outtake() {
+            return new SequentialCommandGroup(
+                new InstantCommand(()-> motor1.set(1), this),
+                new WaitCommand(2),
+                new InstantCommand(()-> motor1.set(0),this)
+            ).withName("AlgaeOuttake");
+         }
+         public Command IntakeandOut() {
+            return new ConditionalCommand(Intake(), Outtake(), sensor::get);
+         }
     
 }
