3468140d19d1a8aa6c82ed00f5235e1e6af01d7f
Gymnast13 <zegilsdorf@gmail.com>
2025-01-28 18:22:50 -0500

    Added pivot subsystem

---
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+@Logged
+
+public class Pivot extends SubsystemBase {
+
+private SparkMax pivotmotor;
+
+  /** Creates a new Pivot. */
+  public Pivot() {
+
+    pivotmotor = new SparkMax(52, MotorType.kBrushless);
+
+    SparkMaxConfig pivotmotorConfig = new SparkMaxConfig();
+
+    pivotmotorConfig
+    .smartCurrentLimit(20)
+    .idleMode(IdleMode.kBrake);
+
+    pivotmotor.configure(pivotmotorConfig,ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putData(this);
+  }
+
+}
