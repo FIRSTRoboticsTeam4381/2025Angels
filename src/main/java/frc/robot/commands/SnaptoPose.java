3e4d0bf9d8077d863bac78bfa05fc2d5c21add1e
BillyBobJones678 <williamholmes678@gmail.com>
2025-02-01 13:29:08 -0500

    A start

--- src/main/java/frc/robot/commands/SnaptoPose.java
+++ src/main/java/frc/robot/commands/SnaptoPose.java
@@ -4,10 +4,25 @@
 
 package frc.robot.commands;
 
+import org.photonvision.EstimatedRobotPose;
+
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Constants.Swerve;
 
 
 public class SnaptoPose extends Command
 {
+EstimatedRobotPose estimatedPose,
+swerveOdometry = new SwerveDrivePoseEstimator(Constants.Swerve.swerveKinematics, getYaw(), getPositions(), startPose);
+}
+private Pose2d startPose = new Pose2d(Units.inchesToMeters(177), Units.inchesToMeters(214), Rotation2d.fromDegrees(0));
 
+
+public Pose2d getPose(){
+    return swerveOdometry.getEstimatedPosition();
 }
