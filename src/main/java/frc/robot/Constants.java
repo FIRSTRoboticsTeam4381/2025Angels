79ee1358c28bb18b2233f6b5ad0dbb5e4f786622
BillyBobJones678 <williamholmes678@gmail.com>
2025-02-04 18:57:04 -0500

    Changed robot configs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,14 +32,14 @@ public final class Constants {
         /* Drivetrain Constants */
         public static final double trackWidth = Units.inchesToMeters(24.75); 
         public static final double wheelBase = Units.inchesToMeters(18.75); 
-        public static final double wheelDiameter = Units.inchesToMeters(3.0 + 7.0/8.8);
+        public static final double wheelDiameter = Units.inchesToMeters(4);
         public static final double wheelCircumference = wheelDiameter * Math.PI;
 
         public static final double openLoopRamp = 0.25;
         public static final double closedLoopRamp = 0.0;
 
-        public static final double driveGearRatio = (425.0 / 72.0); 
-        public static final double angleGearRatio = (150.0/7.0 / 1.0); 
+        public static final double driveGearRatio = (6.75); 
+        public static final double angleGearRatio = (150.0/7.0); 
 
         public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
             new Translation2d(wheelBase / 2.0, trackWidth / 2.0),
@@ -79,8 +79,9 @@ public final class Constants {
         /* Swerve Profiling Values */
         // TODO The robot only seems to be making it up to about 4.5 m/s, could be a conversion issue
         // in code or a result of robot weight/friction/etc decreasing theoretical free speed
-        public static final double maxSpeed = 6; //meters per second
-        public static final double maxAngularVelocity = 11.5;
+        public static final double maxSpeed = 5.36; //meters per second
+        // Spin speed omega radians per sec before it was 11.5
+        public static final double maxAngularVelocity = 10;
 
         /* Motor Inverts */
         public static final boolean driveMotorInvert = true;
@@ -131,7 +132,7 @@ public final class Constants {
         }
     }
 
-    public static final class AutoConstants{
+    /*public static final class AutoConstants{
         public static final double kMaxSpeedMetersPerSecond = 3;
         public static final double kMaxAccelerationMetersPerSecondSquared = 3;
         public static final double kMaxAngularSpeedRadiansPerSecond = 2*Math.PI;
@@ -145,5 +146,7 @@ public final class Constants {
         public static final TrapezoidProfile.Constraints kThetaControllerConstraints = 
             new TrapezoidProfile.Constraints(
                 kMaxAngularSpeedRadiansPerSecond, kMaxAngularAccelerationRadiansPerSecondSquared);
+    
     }
+                */
 }
