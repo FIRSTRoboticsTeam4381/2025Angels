4e18053b2f552f47f2a14210ea5820c8ee37c932
BillyBobJones678 <williamholmes678@gmail.com>
2025-02-07 11:25:45 -0500

    Fixed format on coral intake

--- src/main/java/frc/robot/subsystems/Coralintake.java
+++ src/main/java/frc/robot/subsystems/Coralintake.java
@@ -27,61 +27,65 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.RobotContainer;
 
-
 @Logged
 public class Coralintake extends SubsystemBase {
-//**creating the variables for the motors**
+  // **creating the variables for the motors**
 
   private SparkMax coralmotor1;
   private SparkMax coralmotor2;
 
-  //creating a sensor
+  // creating a sensor
   private SparkLimitSwitch coralsensor1;
   private SparkLimitSwitch coralsensor2;
+
   // Creates a new Coralintake
   public Coralintake() {
 
-  //assign cAn ID and Motor type
-  coralmotor1 = new SparkMax(55,MotorType.kBrushless);
-  coralmotor2 = new SparkMax(56,MotorType.kBrushless);
+    // assign cAn ID and Motor type
+    coralmotor1 = new SparkMax(55, MotorType.kBrushless);
+    coralmotor2 = new SparkMax(56, MotorType.kBrushless);
 
-  //sets the can ID for a sensor
-  coralsensor1 = coralmotor1.getForwardLimitSwitch();
-  coralsensor2 = coralmotor2.getForwardLimitSwitch();
+    // sets the can ID for a sensor
+    coralsensor1 = coralmotor1.getForwardLimitSwitch();
+    coralsensor2 = coralmotor2.getForwardLimitSwitch();
 
-  //set up the config
-  SparkMaxConfig coralmotor1Config = new SparkMaxConfig();
-  NamedCommands.registerCommand("coralinorout", Coralinorout());
-  //assign properties to motor
-  coralmotor1Config
-  .smartCurrentLimit(10)
-  .idleMode(IdleMode.kBrake)
-  .limitSwitch.forwardLimitSwitchEnabled(false);
+    // set up the config
+    SparkMaxConfig coralmotor1Config = new SparkMaxConfig();
+    NamedCommands.registerCommand("coralinorout", Coralinorout());
+    // assign properties to motor
+    coralmotor1Config
+        .smartCurrentLimit(10)
+        .idleMode(IdleMode.kBrake).limitSwitch.forwardLimitSwitchEnabled(false);
 
-  //set whether it will reset parameters when they are changed, and the persist mode
-  coralmotor1.configure(coralmotor1Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // set whether it will reset parameters when they are changed, and the persist
+    // mode
+    coralmotor1.configure(coralmotor1Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-  //making motor4 follow motor3
-  SparkMaxConfig coralmotor2Config = new SparkMaxConfig();
-  coralmotor2Config.apply(coralmotor2Config);
-  coralmotor2Config.follow(coralmotor1//telling motor4 to be a follower of motor3
-);
+    // making motor4 follow motor3
+    SparkMaxConfig coralmotor2Config = new SparkMaxConfig();
+    coralmotor2Config.apply(coralmotor2Config);
+    coralmotor2Config.follow(coralmotor1// telling motor4 to be a follower of motor3
+    );
 
-coralmotor2.configure(coralmotor2Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    coralmotor2.configure(coralmotor2Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-this.setDefaultCommand(
-  //sets default command
+    this.setDefaultCommand(
+        // sets default command
 
-  new FunctionalCommand(
-    //basic functional command
+        new FunctionalCommand(
+            // basic functional command
 
-    ()->coralmotor1.set(0), //oninit
-    ()->{}, //onexecute
-    (killed)->{}, //on end
-    ()->{return false;}, //isfinished
-    this)
-    
-);
+            () -> coralmotor1.set(0), // oninit
+            () -> {
+            }, // onexecute
+            (killed) -> {
+            }, // on end
+            () -> {
+              return false;
+            }, // isfinished
+            this)
+
+    );
 
   }
 
@@ -91,53 +95,40 @@ this.setDefaultCommand(
     SmartDashboard.putData(this);
   }
 
-  public Command CoralOut()
-{//creating a sequential command group
-  return new SequentialCommandGroup(
-    //seting the motor speed to 1
-    new InstantCommand(()-> coralmotor1.set(1),this),
-    //checking to see if the sensor can see the coral
-    new WaitUntilCommand(()->!hascoral()),
-    //wait time after throwing coral out
-    new WaitCommand(1.5),
-    //stopping the motor.
-    new InstantCommand(()->coralmotor1.set(0),this)
-    //giving the action a name for logging/
-  ).withName("Coral Outaking");
+  public Command CoralOut() {// creating a sequential command group
+    return new SequentialCommandGroup(
+        // seting the motor speed to 1
+        new InstantCommand(() -> coralmotor1.set(1), this),
+        // checking to see if the sensor can see the coral
+        new WaitUntilCommand(() -> !hascoral()),
+        // wait time after throwing coral out
+        new WaitCommand(1.5),
+        // stopping the motor.
+        new InstantCommand(() -> coralmotor1.set(0), this)
+    // giving the action a name for logging/
+    ).withName("Coral Outaking");
   };
 
-  public Command Coralin()
-  {
+  public Command Coralin() {
     return new SequentialCommandGroup(
-      //this command will run until the sensor sees the coral
-      new InstantCommand(()-> coralmotor1.set(-1),this),
-      RobotContainer.getRobot().vibrateSpecialistWhile(RumbleType.kRightRumble, 0.5,
-        new WaitUntilCommand(()->hascoral())),
-      RobotContainer.getRobot().vibrateDriverForTime(RumbleType.kBothRumble, 0.8, 0.5),
-      new WaitCommand(0.5),
-      new InstantCommand(()->coralmotor1.set(0),this)
-      ).withName("Coral Intaking");
+        // this command will run until the sensor sees the coral
+        new InstantCommand(() -> coralmotor1.set(-1), this),
+        RobotContainer.getRobot().vibrateSpecialistWhile(RumbleType.kRightRumble, 0.5,
+            new WaitUntilCommand(() -> hascoral())),
+        RobotContainer.getRobot().vibrateDriverForTime(RumbleType.kBothRumble, 0.8, 0.5),
+        new WaitCommand(0.5),
+        new InstantCommand(() -> coralmotor1.set(0), this)).withName("Coral Intaking");
   }
 
-  public Command Coralinorout()
-  {
+  public Command Coralinorout() {
     return new ConditionalCommand(
-      CoralOut(), // this will run when the sensor sees something
-       Coralin(), //this will run when the sensor doesn't see anything
-        this::hascoral).withName("Coral in or out is running"); //this tells it which sensor to use
+        CoralOut(), // this will run when the sensor sees something
+        Coralin(), // this will run when the sensor doesn't see anything
+        this::hascoral).withName("Coral in or out is running"); // this tells it which sensor to use
   }
 
-  public boolean hascoral()
-  {
+  public boolean hascoral() {
     return coralsensor1.isPressed() || coralsensor2.isPressed();
   }
 
-
 }
-
-
-
-
-
-
-
