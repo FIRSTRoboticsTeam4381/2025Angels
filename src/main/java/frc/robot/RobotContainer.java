30f345011c1ca10960517a86cc883d7b2da5df0d
Sam <sam.maddox1@yahoo.com>
2025-02-15 16:24:19 -0500

    Add remaining cameras

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -55,8 +55,8 @@ public class RobotContainer {
   public final Pivot pivot;
   public final PhotonCam camA = new PhotonCam("FL", new Transform3d(new Translation3d(Units.inchesToMeters(13.8), Units.inchesToMeters(-5.2),  Units.inchesToMeters(4.29)), new Rotation3d(0,0.22602,0.69690997)) );
   public final PhotonCam camB = new PhotonCam("FR", new Transform3d(new Translation3d(Units.inchesToMeters(13.8), Units.inchesToMeters(5.2),  Units.inchesToMeters(4.29)), new Rotation3d(0,0.22602,-0.69690997)) );
-  public final PhotonCam camC = new PhotonCam("BL", new Transform3d(new Translation3d(Units.inchesToMeters(-10.375), Units.inchesToMeters(7.3125),  Units.inchesToMeters(8.5)), new Rotation3d(0,.2793,Math.PI-Math.PI/4.0)));
-  public final PhotonCam camD = new PhotonCam("BR", new Transform3d(new Translation3d(Units.inchesToMeters(-10.375), Units.inchesToMeters(7.3125),  Units.inchesToMeters(8.5)), new Rotation3d(0,.2793,Math.PI+Math.PI/4.0)) );
+  public final PhotonCam camC = new PhotonCam("BL", new Transform3d(new Translation3d(Units.inchesToMeters(-13.75), Units.inchesToMeters(10),  Units.inchesToMeters(11.5)), new Rotation3d(0,.2793,Math.PI + Math.PI/4.0)));
+  public final PhotonCam camD = new PhotonCam("BR", new Transform3d(new Translation3d(Units.inchesToMeters(-13.75), Units.inchesToMeters(-10),  Units.inchesToMeters(11.5)), new Rotation3d(0,.2793,Math.PI-Math.PI/4.0)) );
 
   public final AdvancedCommands advancedCommands;
   // Constructor: set up the robot! 
